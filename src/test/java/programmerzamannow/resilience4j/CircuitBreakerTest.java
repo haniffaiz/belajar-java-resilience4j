080309b5d31c6b0b293021187135c711e2323227
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-21 01:04:27 +0700

    circuit breaker registry

--- src/test/java/programmerzamannow/resilience4j/CircuitBreakerTest.java
+++ src/test/java/programmerzamannow/resilience4j/CircuitBreakerTest.java
@@ -2,6 +2,7 @@ package programmerzamannow.resilience4j;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -39,6 +40,31 @@ public class CircuitBreakerTest {
                 .build();
 
         CircuitBreaker circuitBreaker = CircuitBreaker.of("pzn", config);
+
+        for (int i = 0; i < 200; i++) {
+            try {
+                Runnable runnable = CircuitBreaker.decorateRunnable(circuitBreaker, () -> callMe());
+                runnable.run();
+            }catch (Exception e){
+                log.error("Error : {}",e.getMessage());
+            }
+        }
+
+    }
+
+    @Test
+    void circuitBreakerRegistry() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .failureRateThreshold(10f)
+                .slidingWindowSize(10)
+                .minimumNumberOfCalls(10)//sama atau kecil dari sliding windows
+                .build();
+
+        CircuitBreakerRegistry registry = CircuitBreakerRegistry.ofDefaults();
+        registry.addConfiguration("config", config);
+
+        CircuitBreaker circuitBreaker = registry.circuitBreaker("pzn","config");
 
         for (int i = 0; i < 200; i++) {
             try {
