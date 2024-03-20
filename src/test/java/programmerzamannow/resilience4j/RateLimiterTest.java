c6c76086747a9ff98b0ee24747d20e949ff843e1
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-20 07:14:50 +0700

    rate limiter config

--- src/test/java/programmerzamannow/resilience4j/RateLimiterTest.java
+++ src/test/java/programmerzamannow/resilience4j/RateLimiterTest.java
@@ -1,9 +1,11 @@
 package programmerzamannow.resilience4j;
 
 import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
@@ -21,6 +23,30 @@ public class RateLimiterTest {
             Runnable runnable = RateLimiter.decorateRunnable(rateLimiter,()->{
                long result = counter.incrementAndGet();
                log.info("Result {}", result);
+
+            });
+
+            runnable.run();
+        }
+
+
+    }
+
+    @Test
+    void testRateLimiterConfig() {
+        //membatasi jumlah request yang dilakukan
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitForPeriod(100)
+                .limitRefreshPeriod(Duration.ofMinutes(1))
+                .timeoutDuration(Duration.ofSeconds(2))
+                .build();
+
+        RateLimiter rateLimiter = RateLimiter.of("pzn",config);
+
+        for (int i = 0; i < 10_000; i++) {
+            Runnable runnable = RateLimiter.decorateRunnable(rateLimiter,()->{
+                long result = counter.incrementAndGet();
+                log.info("Result {}", result);
 
             });
 
