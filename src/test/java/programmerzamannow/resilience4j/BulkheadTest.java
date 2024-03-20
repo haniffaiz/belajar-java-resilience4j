24472fcf4a397c975fd8b6bf5f67ead5b1f6f2da
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-20 22:32:46 +0700

    bulkhead config

--- src/test/java/programmerzamannow/resilience4j/BulkheadTest.java
+++ src/test/java/programmerzamannow/resilience4j/BulkheadTest.java
@@ -1,11 +1,14 @@
 package programmerzamannow.resilience4j;
 
 import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadConfig;
 import io.github.resilience4j.bulkhead.ThreadPoolBulkhead;
+import io.github.resilience4j.bulkhead.ThreadPoolBulkheadConfig;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
@@ -19,7 +22,7 @@ public class BulkheadTest {
     public void slow(){
         long value = counter.incrementAndGet();
         log.info("Slow : " + value);
-        Thread.sleep(5_000L);
+        Thread.sleep(1_000L);
     }
 
     @Test
@@ -42,5 +45,40 @@ public class BulkheadTest {
             Supplier<CompletionStage<Void>> supplier = ThreadPoolBulkhead.decorateRunnable(bulkhead, () -> slow());
             supplier.get();
         }
+    }
+
+    @Test
+    void testSemaphoreConfig() throws InterruptedException {
+        BulkheadConfig config = BulkheadConfig.custom()
+                .maxConcurrentCalls(5)
+                .maxWaitDuration(Duration.ofSeconds(5))
+                .build();
+
+        Bulkhead bulkhead = Bulkhead.of("pzn", config);
+
+        for (int i = 0; i < 10; i++) {
+            Runnable runnable = Bulkhead.decorateRunnable(bulkhead, () -> slow());
+            new Thread(runnable).start();
+        }
+        Thread.sleep(10_000L);
+    }
+
+    //antrian 5
+    @Test
+    void testThreadPoolConfig() throws InterruptedException {
+        ThreadPoolBulkheadConfig config = ThreadPoolBulkheadConfig.custom()
+                .maxThreadPoolSize(5)
+                .coreThreadPoolSize(5)
+                .queueCapacity(100)
+                .build();
+
+        log.info(String.valueOf(Runtime.getRuntime().availableProcessors()));
+        ThreadPoolBulkhead bulkhead = ThreadPoolBulkhead.of("pzn", config);
+
+        for (int i = 0; i < 20; i++) {
+            Supplier<CompletionStage<Void>> supplier = ThreadPoolBulkhead.decorateRunnable(bulkhead, () -> slow());
+            supplier.get();
+        }
+        Thread.sleep(10_000L);
     }
 }
