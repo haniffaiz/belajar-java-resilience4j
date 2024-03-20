03f99a34bc537e0bec5faa0957faf7c049fe7883
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-21 00:32:58 +0700

    bulkhead registry

--- src/test/java/programmerzamannow/resilience4j/BulkheadTest.java
+++ src/test/java/programmerzamannow/resilience4j/BulkheadTest.java
@@ -1,9 +1,6 @@
 package programmerzamannow.resilience4j;
 
-import io.github.resilience4j.bulkhead.Bulkhead;
-import io.github.resilience4j.bulkhead.BulkheadConfig;
-import io.github.resilience4j.bulkhead.ThreadPoolBulkhead;
-import io.github.resilience4j.bulkhead.ThreadPoolBulkheadConfig;
+import io.github.resilience4j.bulkhead.*;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -74,6 +71,47 @@ public class BulkheadTest {
 
         log.info(String.valueOf(Runtime.getRuntime().availableProcessors()));
         ThreadPoolBulkhead bulkhead = ThreadPoolBulkhead.of("pzn", config);
+
+        for (int i = 0; i < 20; i++) {
+            Supplier<CompletionStage<Void>> supplier = ThreadPoolBulkhead.decorateRunnable(bulkhead, () -> slow());
+            supplier.get();
+        }
+        Thread.sleep(10_000L);
+    }
+
+    @Test
+    void testSemaphoreRegistry() throws InterruptedException {
+        BulkheadConfig config = BulkheadConfig.custom()
+                .maxConcurrentCalls(5)
+                .maxWaitDuration(Duration.ofSeconds(5))
+                .build();
+
+        BulkheadRegistry registry = BulkheadRegistry.ofDefaults();
+        registry.addConfiguration("config", config);
+
+        Bulkhead bulkhead = registry.bulkhead("pzn","config");
+
+        for (int i = 0; i < 10; i++) {
+            Runnable runnable = Bulkhead.decorateRunnable(bulkhead, () -> slow());
+            new Thread(runnable).start();
+        }
+        Thread.sleep(10_000L);
+    }
+
+    @Test
+    void testThreadPoolRegistry() throws InterruptedException {
+        ThreadPoolBulkheadConfig config = ThreadPoolBulkheadConfig.custom()
+                .maxThreadPoolSize(5)
+                .coreThreadPoolSize(5)
+                .queueCapacity(100)
+                .build();
+
+        log.info(String.valueOf(Runtime.getRuntime().availableProcessors()));
+
+        ThreadPoolBulkheadRegistry registry = ThreadPoolBulkheadRegistry.ofDefaults();
+        registry.addConfiguration("config", config);
+
+        ThreadPoolBulkhead bulkhead = registry.bulkhead("pzn", "config");
 
         for (int i = 0; i < 20; i++) {
             Supplier<CompletionStage<Void>> supplier = ThreadPoolBulkhead.decorateRunnable(bulkhead, () -> slow());
